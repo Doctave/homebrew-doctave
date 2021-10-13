38e5cc985106cba706ce26a3dceba41687ca2a4c
Niklas Begley <niklas.begley@gmail.com>
2021-10-13 16:10:55 +0300

    Update to 0.2.1

--- doctave.rb
+++ doctave.rb
@@ -1,8 +1,8 @@
 class Doctave < Formula
   desc "Opinionated developer documentation site generator"
   homepage "https://cli.doctave.com"
-  url "https://github.com/Doctave/doctave/archive/0.2.0.tar.gz"
-  sha256 "f8d11e8d5419feefb69cd912d32ce80a75d3ca3dcb88500a3a9b8b8f9f10b1d5"
+  url "https://github.com/Doctave/doctave/archive/0.2.1.tar.gz"
+  sha256 "788b6712b6f9c877cb2cea366987af3c13c7e0c8d766810e1442967542da66d7"
   license "MIT"
 
   livecheck do
