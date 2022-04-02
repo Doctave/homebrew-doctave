3b8a20dff83e6db40941d7ff2a6a7a53b2a2bd73
Niklas Begley <niklas.begley@gmail.com>
2022-04-02 09:46:58 +0300

    Bump to version 0.4.2

--- doctave.rb
+++ doctave.rb
@@ -1,8 +1,8 @@
 class Doctave < Formula
   desc "Opinionated developer documentation site generator"
   homepage "https://cli.doctave.com"
-  url "https://github.com/Doctave/doctave/archive/0.4.1.tar.gz"
-  sha256 "e1fd55219a5068c36de03c82c7e8b2b49506f15b564bd50a795a3e79ecb3a784"
+  url "https://github.com/Doctave/doctave/archive/0.4.2.tar.gz"
+  sha256 "8994529738ec9b39d35f916a081f021e481f068546512813e41af8337a4eab97"
   license "MIT"
 
   livecheck do
